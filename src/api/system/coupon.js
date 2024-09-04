9e9ec3f3f3370baef0f77303612a0f1614278e49
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2024-09-04 20:15:39 +0800

    feat: 增加卡券管理，未完成

---
+++ src/api/system/coupon.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询卡券列表
+export function listCoupon(query) {
+  return request({
+    url: '/system/coupon/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询卡券详细
+export function getCoupon(couponId) {
+  return request({
+    url: '/system/coupon/' + couponId,
+    method: 'get'
+  })
+}
+
+// 新增卡券
+export function addCoupon(data) {
+  return request({
+    url: '/system/coupon',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改卡券
+export function updateCoupon(data) {
+  return request({
+    url: '/system/coupon',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除卡券
+export function delCoupon(couponId) {
+  return request({
+    url: '/system/coupon/' + couponId,
+    method: 'delete'
+  })
+}

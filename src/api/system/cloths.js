3be8740cb8321e2b9b8d4b4c54b3cce61f04927e
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2024-09-13 11:05:57 +0800

    feat: 添加衣物模块

---
+++ src/api/system/cloths.js
@@ -0,0 +1,59 @@
+import request from '@/utils/request'
+
+// 查询订单包含的衣物清单列表
+export function listCloths(query) {
+  return request({
+    url: '/system/cloths/list',
+    method: 'get',
+    params: query
+  })
+}
+// 查询订单包含的衣物清单列表
+export function listHistoryCloths(userId) {
+  return request({
+    url: '/system/cloths/list/' + userId,
+    method: 'get',
+  })
+}
+
+// 查询订单包含的衣物清单详细
+export function getCloths(orderClothId) {
+  return request({
+    url: '/system/cloths/' + orderClothId,
+    method: 'get'
+  })
+}
+
+// 查询订单包含的衣物清单详细
+export function getPic(picId) {
+  return request({
+    url: '/system/cloths/download/' + picId,
+    method: 'get'
+  })
+}
+
+// 新增订单包含的衣物清单
+export function addCloths(data) {
+  return request({
+    url: '/system/cloths',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改订单包含的衣物清单
+export function updateCloths(data) {
+  return request({
+    url: '/system/cloths',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除订单包含的衣物清单
+export function delCloths(orderClothId) {
+  return request({
+    url: '/system/cloths/' + orderClothId,
+    method: 'delete'
+  })
+}

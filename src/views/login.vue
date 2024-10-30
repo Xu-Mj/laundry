15955282867a240a63072e25e9f40c6813f6df5c
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2024-10-30 13:56:31 +0800

    feat: 删除用户名密码自动填充

--- src/views/login.vue
+++ src/views/login.vue
@@ -76,8 +76,8 @@ const router = useRouter();
 const { proxy } = getCurrentInstance();
 
 const loginForm = ref({
-  username: "admin",
-  password: "admin123",
+  username: null,
+  password: null,
   rememberMe: false,
   code: "",
   uuid: ""

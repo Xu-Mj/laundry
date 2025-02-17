4a7cd2edfb455dabe70c60ac6c5f8d89ffa792f2
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-02-17 20:40:58 +0800

    feat: 新增第一次登录必须修改密码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,55 +1,31 @@
 <template>
-  <CloseBar position="absolute"/>
+  <CloseBar position="absolute" />
   <div class="login">
     <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
       <h3 class="title">洗衣店</h3>
       <el-form-item prop="username">
-        <el-input
-          v-model="loginForm.username"
-          type="text"
-          size="large"
-          auto-complete="off"
-          placeholder="账号"
-        >
+        <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
           <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
-          type="password"
-          size="large"
-          auto-complete="off"
-          placeholder="密码"
-          @keyup.enter="handleLogin"
-        >
+        <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" placeholder="密码"
+          @keyup.enter="handleLogin">
           <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
         </el-input>
       </el-form-item>
       <el-form-item prop="code" v-if="captchaEnabled">
-        <el-input
-          v-model="loginForm.code"
-          size="large"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter="handleLogin"
-        >
+        <el-input v-model="loginForm.code" size="large" auto-complete="off" placeholder="验证码" style="width: 63%"
+          @keyup.enter="handleLogin">
           <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
         </el-input>
         <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+          <img :src="codeUrl" @click="getCode" class="login-code-img" />
         </div>
       </el-form-item>
       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
       <el-form-item style="width:100%;">
-        <el-button
-          :loading="loading"
-          size="large"
-          type="primary"
-          style="width:100%;"
-          @click.prevent="handleLogin"
-        >
+        <el-button :loading="loading" size="large" type="primary" style="width:100%;" @click.prevent="handleLogin">
           <span v-if="!loading">登 录</span>
           <span v-else>登 录 中...</span>
         </el-button>
@@ -63,6 +39,20 @@
       <!-- <span>Copyright © 2018-2024 ruoyi.vip All Rights Reserved.</span> -->
     </div>
   </div>
+  <el-dialog v-model="pwdDialogVisible" title="首次登录修改密码" :close-on-click-modal="false" :show-close="false"
+    width="500px">
+    <el-form ref="pwdFormRef" :model="pwdForm" :rules="pwdRules" label-width="80px">
+      <el-form-item label="新密码" prop="newPassword">
+        <el-input v-model="pwdForm.newPassword" type="password" show-password placeholder="请输入新密码" />
+      </el-form-item>
+      <el-form-item label="确认密码" prop="confirmPassword">
+        <el-input v-model="pwdForm.confirmPassword" type="password" show-password placeholder="请确认新密码" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button type="primary" @click="submitNewPwd">提交</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
@@ -100,8 +90,64 @@ const register = ref(false);
 const redirect = ref(undefined);
 
 watch(route, (newRoute) => {
-    redirect.value = newRoute.query && newRoute.query.redirect;
+  redirect.value = newRoute.query && newRoute.query.redirect;
 }, { immediate: true });
+
+const pwdDialogVisible = ref(false);
+const pwdForm = ref({
+  newPassword: '',
+  confirmPassword: ''
+});
+
+// 密码验证规则
+const pwdRules = {
+  newPassword: [
+    { required: true, message: '请输入新密码' },
+    { min: 6, message: '密码长度不能少于6位' }
+  ],
+  confirmPassword: [
+    { required: true, message: '请确认新密码' },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== pwdForm.value.newPassword) {
+          callback(new Error('两次输入密码不一致'));
+        } else {
+          callback();
+        }
+      }
+    }
+  ]
+};
+
+// 提交新密码
+const submitNewPwd = async () => {
+  try {
+    await proxy.$refs.pwdFormRef.validate();
+    await userStore.updatePassword({
+      account: loginForm.value.username,
+      oldPassword: loginForm.value.password,
+      newPassword: pwdForm.value.newPassword,
+      confirmPassword: pwdForm.value.confirmPassword,
+    });
+    pwdDialogVisible.value = false;
+    proxy.$modal.msgSuccess("密码修改成功，请重新登录");
+
+    pwdForm.value.newPassword = '';
+    pwdForm.value.confirmPassword = '';
+
+    // 清空登录信息
+    loading.value = false;
+    loginForm.value.password = '';
+    if (loginForm.value.rememberMe) {
+      Cookies.set("password", encrypt(pwdForm.value.newPassword)); // 更新记住的密码
+    }
+
+    // 强制跳转登录页
+    router.replace('/login');
+  } catch (err) {
+    proxy.$modal.msgError(err.message || '密码修改失败');
+  }
+};
 
 function handleLogin() {
   proxy.$refs.loginRef.validate(valid => {
@@ -119,15 +165,12 @@ function handleLogin() {
         Cookies.remove("rememberMe");
       }
       // 调用action的登录方法
-      userStore.login(loginForm.value).then(() => {
-        const query = route.query;
-        const otherQueryParams = Object.keys(query).reduce((acc, cur) => {
-          if (cur !== "redirect") {
-            acc[cur] = query[cur];
-          }
-          return acc;
-        }, {});
-        router.push({ path: redirect.value || "/", query: otherQueryParams });
+      userStore.login(loginForm.value).then((res) => {
+        if (res.user.isFirstLogin) { // 根据实际响应字段判断
+          pwdDialogVisible.value = true; // 显示修改密码弹窗
+        } else {
+          routeJump();
+        }
       }).catch(() => {
         loading.value = false;
         // 重新获取验证码
@@ -137,6 +180,18 @@ function handleLogin() {
       });
     }
   });
+}
+
+function routeJump() {
+  // 原有跳转逻辑
+  const query = route.query;
+  const otherQueryParams = Object.keys(query).reduce((acc, cur) => {
+    if (cur !== "redirect") {
+      acc[cur] = query[cur];
+    }
+    return acc;
+  }, {});
+  router.push({ path: redirect.value || "/", query: otherQueryParams });
 }
 
 function getCode() {
@@ -173,6 +228,7 @@ getCookie();
   background-image: url("../assets/images/login-background.jpg");
   background-size: cover;
 }
+
 .title {
   margin: 0px auto 30px auto;
   text-align: center;
@@ -184,33 +240,40 @@ getCookie();
   background: #ffffff;
   width: 400px;
   padding: 25px 25px 5px 25px;
+
   .el-input {
     height: 40px;
+
     input {
       height: 40px;
     }
   }
+
   .input-icon {
     height: 39px;
     width: 14px;
     margin-left: 0px;
   }
 }
+
 .login-tip {
   font-size: 13px;
   text-align: center;
   color: #bfbfbf;
 }
+
 .login-code {
   width: 33%;
   height: 40px;
   float: right;
   overflow: hidden;
+
   img {
     cursor: pointer;
     vertical-align: middle;
   }
 }
+
 .el-login-footer {
   height: 40px;
   line-height: 40px;
@@ -223,6 +286,7 @@ getCookie();
   font-size: 12px;
   letter-spacing: 1px;
 }
+
 .login-code-img {
   height: 40px;
   padding-left: 12px;

99176c3f89b929f408e9a3094971958436966d84
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-03-16 17:57:18 +0800

    feat: reset style

--- src/components/DictTag/index.vue
+++ src/components/DictTag/index.vue
@@ -2,20 +2,12 @@
   <div>
     <template v-for="(item, index) in options">
       <template v-if="values.includes(item.value)">
-        <!-- <span
+        <span
           v-if="(item.elTagType == 'default' || item.elTagType == '') && (item.elTagClass == '' || item.elTagClass == null)"
-          :key="item.value"
-          :index="index"
-          :class="item.elTagClass"
-        >{{ item.label + " " }}</span> -->
-        <el-tag
-          :disable-transitions="true"
-          :key="item.value + ''"
-          :index="index"
-          :type="item.elTagType"
-          :class="item.elTagClass" 
-          :effect="item.elTagType == 'primary'? 'plain':''"
-        >{{ item.label + " " }}</el-tag>
+          :key="item.value" :index="index" :class="item.elTagClass">{{ item.label + " " }}</span>
+        <el-tag v-else :disable-transitions="true" :key="item.value + ''" :index="index"
+          :type="item.elTagType === 'primary' ? '' : item.elTagType" :class="item.elTagClass">{{ item.label + " "
+          }}</el-tag>
       </template>
     </template>
     <template v-if="unmatch && showValue">

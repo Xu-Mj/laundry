db7a75e558201033dc59615baa432141c63206e5
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-03-16 11:20:47 +0800

    feat: move water mark here

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,36 +1,30 @@
 <template>
-  <SidebarNew class="sidebar-container" />
-  <div :class="{ hasTagsView: needTagsView, sidebarHide: sidebar.hide }" class="main-container">
-    <CloseBar />
-    <app-main />
-  </div>
+  <AppWatermark>
+    <SidebarNew class="sidebar-container" />
+    <div :class="{ hasTagsView: needTagsView, sidebarHide: sidebar.hide }" class="main-container">
+      <CloseBar />
+      <app-main />
+    </div>
+  </AppWatermark>
 </template>
 
 <script setup>
 import { useWindowSize } from '@vueuse/core'
 import SidebarNew from './components/SidebarNew/index.vue'
-import { AppMain, Settings } from './components'
+import { AppMain } from './components'
 import CloseBar from '@/components/close_bar'
+import AppWatermark from '@/components/AppWatermark/index.vue'
 
 import useAppStore from '@/store/modules/app'
 import useSettingsStore from '@/store/modules/settings'
+import { onMounted } from 'vue'
 
 const settingsStore = useSettingsStore()
-const theme = computed(() => settingsStore.theme);
-const sideTheme = computed(() => settingsStore.sideTheme);
 const sidebar = computed(() => useAppStore().sidebar);
 const device = computed(() => useAppStore().device);
 const needTagsView = computed(() => settingsStore.tagsView);
-const fixedHeader = computed(() => settingsStore.fixedHeader);
 
-const classObj = computed(() => ({
-  hideSidebar: !sidebar.value.opened,
-  openSidebar: sidebar.value.opened,
-  withoutAnimation: sidebar.value.withoutAnimation,
-  mobile: device.value === 'mobile'
-}))
-
-const { width, height } = useWindowSize();
+const { width } = useWindowSize();
 const WIDTH = 992; // refer to Bootstrap's responsive design
 
 watch(() => device.value, () => {
@@ -47,16 +41,6 @@ watchEffect(() => {
     useAppStore().toggleDevice('desktop')
   }
 })
-
-function handleClickOutside() {
-  useAppStore().closeSideBar({ withoutAnimation: false })
-}
-
-const settingRef = ref(null);
-function setLayout() {
-  console.log('setLayout')
-  settingRef.value.openSetting();
-}
 </script>
 
 <style lang="scss" scoped>
@@ -73,36 +57,5 @@ function setLayout() {
     position: fixed;
     top: 0;
   }
-}
-
-.drawer-bg {
-  background: #000;
-  opacity: 0.3;
-  width: 100%;
-  top: 0;
-  height: 100%;
-  position: absolute;
-  z-index: 999;
-}
-
-.fixed-header {
-  position: fixed;
-  top: 0;
-  right: 0;
-  z-index: 9;
-  width: calc(100% - #{$base-sidebar-width});
-  transition: width 0.28s;
-}
-
-.hideSidebar .fixed-header {
-  width: calc(100% - 54px);
-}
-
-.sidebarHide .fixed-header {
-  width: 100%;
-}
-
-.mobile .fixed-header {
-  width: 100%;
 }
 </style>

983f0e618e61bd5a7b8a2f0bdc70e6a8d932e74b
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-03-16 19:22:38 +0800

    feat: remove theme setting

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,6 @@
     <router-view />
 </template>
 
-<script setup>
-import useSettingsStore from '@/store/modules/settings'
-import { handleThemeStyle } from '@/utils/theme'
-
-onMounted(() => {
-  nextTick(() => {
-    // 初始化主题样式
-    handleThemeStyle(useSettingsStore().theme)
-  })
-})
-</script>
-
 <style>
 :root {
   view-transition-name: root;

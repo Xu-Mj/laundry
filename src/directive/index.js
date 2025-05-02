7cdffc7010551ecd2461e14dbcaca6c02bdd937d
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-02 14:50:49 +0800

    feat: remove comments

--- src/directive/index.js
+++ src/directive/index.js
@@ -1,9 +1,7 @@
 import copyText from './common/copyText'
-import vSlideIn from '@/vSlideIn'
+import vSlideIn from './vSlideIn'
 
-export default function directive(app){
-  // app.directive('hasRole', hasRole)
-  // app.directive('hasPermi', hasPermi)
+export default function directive(app) {
   app.directive('copyText', copyText)
   app.directive('slide-in', vSlideIn)
 }

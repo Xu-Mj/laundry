04b3c5a0a6c088f5783811cd6a96a02c9029fe79
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-08 11:30:53 +0800

    feat: adjust config

--- src/api/system/config.js
+++ src/api/system/config.js
@@ -1,18 +1,13 @@
 import invoke from '@/utils/invoke'
 
 // 查询参数列表
-export function listConfig(query) {
-  const pageParams = { pageSize: query.pageSize, page: query.pageNum, params: query.params };
-  const config = {
-    configType: query.configType,
-    configName: query.configName,
-  }
-  return invoke('get_config_list', { pageParams: pageParams, config: config })
+export function listConfig() {
+  return invoke('get_config_list', { pageParams: {}, config: {} })
 }
 
 // 查询参数详细
 export function getConfig(configId) {
- return invoke('get_config_by_id', { id: configId })
+  return invoke('get_config_by_id', { id: configId })
 }
 
 // 根据参数键名查询参数值

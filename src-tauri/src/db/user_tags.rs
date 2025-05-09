bc51fe5d5d537c1116ff51b829678fc88f10f54c
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-09 14:41:08 +0800

    feat: delete store id attr

--- src-tauri/src/db/user_tags.rs
+++ src-tauri/src/db/user_tags.rs
@@ -11,7 +11,6 @@ pub struct UserTags {
     pub user_id: Option<i64>,
     pub tags: Option<String>,
     pub remark: Option<String>,
-    pub store_id: Option<i64>,
 }
 
 impl FromRow<'_, SqliteRow> for UserTags {
@@ -20,18 +19,16 @@ impl FromRow<'_, SqliteRow> for UserTags {
             user_id: row.try_get("user_id").unwrap_or_default(),
             tags: row.try_get("tags").unwrap_or_default(),
             remark: row.try_get("remark").unwrap_or_default(),
-            store_id: row.try_get("store_id").unwrap_or_default(),
         })
     }
 }
 
 impl UserTags {
-    pub fn new(user_id: i64, tags: String, remark: Option<String>, store_id: Option<i64>) -> Self {
+    pub fn new(user_id: i64, tags: String, remark: Option<String>) -> Self {
         Self {
             user_id: Some(user_id),
             tags: Some(tags),
             remark,
-            store_id,
         }
     }
 }
@@ -41,25 +38,19 @@ impl UserTags {
     pub async fn get_by_user_id(
         tr: &mut Transaction<'_, Sqlite>,
         id: i64,
-        store_id: Option<i64>,
     ) -> Result<Option<Self>> {
         let mut query = sqlx::QueryBuilder::new("SELECT * FROM user_tags WHERE user_id = ");
         query.push_bind(id);
-
-        if let Some(store_id) = store_id {
-            query.push(" AND store_id = ").push_bind(store_id);
-        }
 
         let tag = query.build_query_as().fetch_optional(&mut **tr).await?;
         Ok(tag)
     }
 
     pub async fn insert(&self, tr: &mut Transaction<'_, Sqlite>) -> Result<()> {
-        sqlx::query("INSERT INTO user_tags (user_id, tags, remark, store_id) VALUES (?, ?, ?, ?)")
+        sqlx::query("INSERT INTO user_tags (user_id, tags, remark) VALUES (?, ?, ?)")
             .bind(self.user_id)
             .bind(&self.tags)
             .bind(&self.remark)
-            .bind(&self.store_id)
             .execute(&mut **tr)
             .await?;
         Ok(())
@@ -67,17 +58,15 @@ impl UserTags {
 
     pub async fn update(&self, tr: &mut Transaction<'_, Sqlite>) -> Result<()> {
         sqlx::query(
-            "INSERT INTO user_tags (user_id, tags, remark, store_id)
-                        VALUES (?, ?, ?, ?)
+            "INSERT INTO user_tags (user_id, tags, remark)
+                        VALUES (?, ?, ?)
                         ON CONFLICT(user_id) DO UPDATE SET
                         tags = excluded.tags,
-                        remark = excluded.remark,
-                        store_id = excluded.store_id",
+                        remark = excluded.remark",
         )
         .bind(self.user_id)
         .bind(&self.tags)
         .bind(&self.remark)
-        .bind(&self.store_id)
         .execute(&mut **tr)
         .await?;
         Ok(())
@@ -87,14 +76,9 @@ impl UserTags {
     pub async fn delete_by_user_id(
         tr: &mut Transaction<'_, Sqlite>,
         user_id: i64,
-        store_id: Option<i64>,
     ) -> Result<()> {
         let mut query = sqlx::QueryBuilder::new("DELETE FROM user_tags WHERE user_id = ");
         query.push_bind(user_id);
-
-        if let Some(store_id) = store_id {
-            query.push(" AND store_id = ").push_bind(store_id);
-        }
 
         query.build().execute(&mut **tr).await?;
         Ok(())

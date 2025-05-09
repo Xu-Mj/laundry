1c14b50b9ffec03dd112824a4c061e7fefff9e4a
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-09 14:39:18 +0800

    feat: cancel jump to home

--- src/views/frontend/container/createOrderContainer.vue
+++ src/views/frontend/container/createOrderContainer.vue
@@ -1,9 +1,8 @@
 <template>
-    <CreateOrder ref="createOrderRef" :visible="true" :orderId="0" :userId="0" :toggle="() => { router.push('/index') }"
+    <CreateOrder ref="createOrderRef" :visible="true" :orderId="0" :userId="0" :toggle="() => {  }"
         :refresh="() => { }" />
 </template>
 
 <script setup>
 import CreateOrder from '@/views/components/createOrder.vue';
-const router = useRouter();
 </script>

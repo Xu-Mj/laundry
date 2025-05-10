c46df7f847a70a7f75462e0b1837a82797532f2a
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-10 21:49:32 +0800

    feat: roll back

--- src/views/frontend/container/createOrderContainer.vue
+++ src/views/frontend/container/createOrderContainer.vue
@@ -1,8 +1,9 @@
 <template>
-    <CreateOrder ref="createOrderRef" :visible="true" :orderId="0" :userId="0" :toggle="() => {  }"
+    <CreateOrder ref="createOrderRef" :visible="true" :orderId="0" :userId="0" :toggle="() => { router.push('/index') }"
         :refresh="() => { }" />
 </template>
 
 <script setup>
 import CreateOrder from '@/views/components/createOrder.vue';
+const router = useRouter();
 </script>

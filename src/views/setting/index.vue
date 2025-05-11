1d6ac329c27f985b33f12409450a24992f40033e
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-11 21:22:02 +0800

    feat: add another printer configuration

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,7 +1,11 @@
 <template>
     <el-dialog title="选择打印机" v-model="open" width="400px" :show-close="false" append-to-body
     :align-center="true" @closed="closeHangUpDialog">
-        <el-select v-model="printer" placeholder="请选择打印机" @change="set">
+        <el-select v-model="printerType" placeholder="请选择打印机类型" @change="fetchSettledPrinter">
+            <el-option label="商家自用" value="business" />
+            <el-option label="顾客小票" value="receipt" />
+        </el-select>
+        <el-select v-model="printer" placeholder="请选择打印机" @change="set" style="margin-top: 1rem;">
             <el-option v-for="item in printers" :key="item.name" :label="item.name" :value="item.name"> </el-option>
         </el-select>
     </el-dialog>
@@ -24,6 +28,7 @@ const props = defineProps({
 const {proxy} = getCurrentInstance();
 const open = ref(false);
 const printer = ref();
+const printerType = ref('business');
 const settledPrinter = ref();
 const printers = ref([]);
 
@@ -37,7 +42,7 @@ function closeHangUpDialog() {
 async function set() {
     settledPrinter.value = printers.value.find(item => item.name === printer.value);
     try {
-        await setPrinter(settledPrinter.value);
+        await setPrinter({ ...settledPrinter.value, printer_type: printerType.value });
         proxy.notify.success("设置成功");
     } catch (error) {
         console.error(error);
@@ -45,13 +50,17 @@ async function set() {
     }
 }
 
+async function fetchSettledPrinter() {
+    settledPrinter.value = await getSettledPrinter(printerType.value);
+    printers.value = await getPrinters();
+    if (settledPrinter.value) {
+        printer.value = settledPrinter.value.name;
+    }
+}
+
 onMounted(async () => {
     if (props.visible) {
-        settledPrinter.value = await getSettledPrinter();
-        printers.value = await getPrinters();
-        if (settledPrinter.value) {
-            printer.value = settledPrinter.value.name;
-        }
+        await fetchSettledPrinter();
         open.value = true;
     }
 })

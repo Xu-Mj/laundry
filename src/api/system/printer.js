52a4328da283aef721faa63b2ce4ef2fe06a366d
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-11 21:21:01 +0800

    feat: add print receipt api

--- src/api/system/printer.js
+++ src/api/system/printer.js
@@ -8,10 +8,22 @@ export function setPrinter(printer) {
   return invoke('set_printer', {printer})
 }
 
-export function getSettledPrinter() {
-  return invoke('get_settled_printer')
+export function getSettledPrinter(printerType) {
+  return invoke('get_settled_printer', {printerType})
 }
 
 export function getPrinters() {
   return invoke('get_printers')
+}
+
+export function printReceipt(data) {
+  const {clothList, mount, paymentMethod, ...order} = data;
+
+  return invoke('print_receipt', {order:{ order, mount, paymentMethod, clothes: clothList }});
+}
+
+export function printReceipt2(data) {
+  const {cloths, mount, paymentMethod, ...order} = data;
+
+  return invoke('print_receipt', {order:{ order, mount, paymentMethod, clothes: cloths }});
 }

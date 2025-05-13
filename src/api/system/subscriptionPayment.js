963c951cc55963b4cb48a7bb5b64c70b79f0e0a1
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-13 11:48:12 +0800

    feat: add wechat api

--- src/api/system/subscriptionPayment.js
+++ src/api/system/subscriptionPayment.js
@@ -11,20 +11,32 @@ export function querySubscriptionPaymentWithAlipay(data) {
   return invoke('check_alipay_qr_code_payment_status', { req: data })
 }
 
-// 发起订阅支付请求
+// wechat
 export function createSubscriptionPaymentWithWechat(data) {
-  return request({
-    url: '/payment/subscription/wechat',
-    method: 'post',
-    data: data
-  })
+  return invoke('get_wechat_qr_code', { req: data })
 }
 
 // 查询订阅支付状态
 export function querySubscriptionPaymentWithWechat(data) {
-  return request({
-    url: '/payment/subscription/wechat/query',
-    method: 'post',
-    data: data
-  })
+  return invoke('check_wechat_qr_code_payment_status', { req: data })
+}
+
+// 发起订阅支付请求
+export function createSmsSubPaymentWithAlipay(data) {
+  return invoke('get_alipay_sms_sub_qr_code', { req: data })
+}
+
+// 查询订阅支付状态
+export function querySmsSubPaymentWithAlipay(data) {
+  return invoke('check_alipay_sms_sub_qr_code_payment_status', { req: data })
+}
+
+// wechat
+export function createSmsSubPaymentWithWechat(data) {
+  return invoke('get_wechat_sms_sub_qr_code', { req: data })
+}
+
+// 查询订阅支付状态
+export function querySmsSubPaymentWithWechat(data) {
+  return invoke('check_wechat_sms_sub_qr_code_payment_status', { req: data })
 }

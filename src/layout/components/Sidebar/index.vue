02bdd4eb5ce70f5a8f44067f14dc66694be6c751
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-14 17:16:09 +0800

    feat: update name

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -94,7 +94,7 @@ const manageMenus = [
   { 'name': '通知记录', 'type': 'primary', show: true, path: '/system/notice-record', onClick: () => router.push('/system/notice-record') },
   { 'name': '参数设置', 'type': 'primary', show: true, path: '/system/config', onClick: () => router.push('/system/config') },
   { 'name': '字典管理', 'type': 'primary', show: false, path: '/system/dict', onClick: () => router.push('/system/dict') },
-  { 'name': '衣物分类', 'type': 'primary', show: true, path: '/system/clothingCategory', onClick: () => router.push('/system/clothingCategory') },
+  { 'name': '衣物品类', 'type': 'primary', show: true, path: '/system/clothingCategory', onClick: () => router.push('/system/clothingCategory') },
 ];
 
 const menus = computed(() => {

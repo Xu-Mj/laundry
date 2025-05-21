92b660e78e4b444f9253331543bdf28f7c9fa550
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-05-21 23:07:12 +0800

    feat: adjust close bar

--- src/components/close_bar.vue
+++ src/components/close_bar.vue
@@ -1,28 +1,44 @@
 <template>
     <div data-tauri-drag-region class="close-bar">
         <el-button link icon="Minus" @click="minimizeWindow" />
-        <el-button v-if="windowStore.showMaximizeButton" link :icon="windowStore.isMaximized ? 'FullScreen' : 'FullScreen'"
+        <el-button v-if="showMaximizeButton" link :icon="isMaximized ? 'FullScreen' : 'Crop'"
             @click="maximizeOrRestoreWindow" />
         <el-button link icon="Close" @click="closeWindow" />
     </div>
 </template>
 
 <script setup name="CloseBar">
-import { useWindowStore } from '@/store/modules/window';
 import useUserStore from '@/store/modules/user';
 import { removeToken } from '@/utils/auth';
+import { getCurrentWindow } from '@tauri-apps/api/window';
 
-const windowStore = useWindowStore();
+const window = getCurrentWindow();
 const userStore = useUserStore();
 
+const showMaximizeButton = computed(async () => {
+    try {
+        // 获取当前显示器信息
+        const size = await window.innerSize();
+        const screenWidth = size.width;
+        const screenHeight = size.height;
+
+        return screenWidth < 1920 || screenHeight < 1080;
+    } catch (error) {
+        console.error('设置窗口大小时出错:', error);
+    }
+    return false;
+});
+
+const isMaximized = ref(window.isMaximized());
 // 窗口最小化
 const minimizeWindow = () => {
-    windowStore.minimize();
+    window.minimize();
 };
 
 // 窗口最大化或还原
-const maximizeOrRestoreWindow = () => {
-    windowStore.toggleMaximize();
+const maximizeOrRestoreWindow = async () => {
+    isMaximized.value = await window.isMaximized();
+    window.toggleMaximize();
 };
 
 // 关闭窗口
@@ -30,10 +46,10 @@ const closeWindow = async () => {
     try {
         await userStore.logOut();
         removeToken();
-        windowStore.close();
+        window.close();
     } catch (error) {
         console.error('关闭窗口时出错:', error);
-        windowStore.close();
+        window.close();
     }
 };
 </script>
